4cbbac790081b95fc4cfe18221890df25d0d1f23
Anish <anishrockagarwal1024@gmail.com>
2022-06-27 21:56:37 +0530

    video streaming enabled

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+navigator.getUserMedia(
+  { video: true, audio: true },
+  stream => {
+    const localVideo = document.getElementById("local-video");
+    console.log(localVideo);
+    if (localVideo) {
+      localVideo.srcObject = stream;
+    }
+  },
+  error => {
+    console.warn(error.message);
+  }
+ );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video autoPlay muted id="local-video">
+      </video>
     </div>
   );
 }
